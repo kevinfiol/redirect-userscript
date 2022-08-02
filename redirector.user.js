7306fd7d107233a969fcccf5967c202041521bd8
kevinfiol <me@kevinfiol.com>
2022-08-01 21:01:13 -0400

    Create redirector.user.js

---
+++ redirector.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        redirector
+// @namespace   Violentmonkey Scripts
+// @match       https://*.imgur.com/*
+// @match       https://*.twitter.com/*
+// @match       https://*.medium.com/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 7/30/2022, 5:34:35 PM
+// ==/UserScript==
+
+let site = window.location.hostname.split('.').at(-2);
+let path = window.location.pathname;
+let query = window.location.search;
+
+// add redirects here
+// omit TLDs from original hostnames as shown below
+// don't forget to add a @match clause to your `==UserScript==` block above
+let redirects = {
+  'imgur': 'i.bcow.xyz',
+  'twitter': 'notabird.site',
+  'medium': 'scribe.rip'
+};
+
+let redirect = redirects[site];
+if (redirect) window.location = `https://${redirect}${path}${query}`;
